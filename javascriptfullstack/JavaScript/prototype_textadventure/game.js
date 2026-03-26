b6e21d14a81afdc4ddc15cd1051ab81c31470561
elfivoid <elisa@aulweb.de>
2026-03-26 16:02:30 +0100

    TextAdventureRefactoring
    refactored the nodetree

--- javascriptfullstack/JavaScript/prototype_textadventure/game.js
+++ javascriptfullstack/JavaScript/prototype_textadventure/game.js
@@ -9,17 +9,23 @@ let state = {}
 // -------- START --------
 function startGame() {
     state = {}
-    showTextNode(1)
+    showTextNode(Node.start)
 }
 
 // -------- TEXTNODE SHOW --------
 function showTextNode(textNodeIndex) {
-    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+    const textNode = textNodes[textNodeIndex]
+// Error handling for missing text node
+    if (!textNode) {
+        console.error("Text node not found:", textNodeIndex)
+        return
+    }
+// Update the text content
     textElement.innerText = textNode.text
     while (optionButtonsElement.firstChild) {
         optionButtonsElement.replaceChildren()
     }
-
+// Create buttons for each option
     textNode.options.forEach(option => {
         if (showOption(option)) {
             const button = document.createElement("button")
@@ -30,71 +36,143 @@ function showTextNode(textNodeIndex) {
         }
     })
 }
-
+// -------- OPTION SHOW --------
 function showOption(option) {
     return option.requiredState == null || option.requiredState(state)
 }
 
 // -------- OPTION --------
 function selectOption(option) {
-    const nextTextNodeId = option.nextText
-    state = Object.assign(...state, ...arguments(option.setState ?? {}))
-    showTextNode(nextTextNodeId)
+    const nextNodeNodeId = option.nextNode
+    const newState = { ...state, ...(option.setState ?? {}) }
+    state = newState
+    showTextNode(nextNodeNodeId)
 }
-// -------- TEXTNODES --------
-const textNodes = [
-    {
-       id: 1,
+
+// -------- TEXTNODES OVERVIEW --------
+const Node = {
+  start: "start",
+  merchant: "merchant",
+            merchant_sword: "merchant_sword",
+            merchant_shield: "merchant_shield",
+            merchant_swordShield: "merchant_swordShield",
+            merchant_ignore: "merchant_ignore",
+    sword: "sword",
+  win: "win",
+  lose: "lose"
+};
+
+// -------- TEXTNODES TREE --------
+const textNodes = {
+
+   [Node.start] : {
        text: "You wake up in a strange place and see a pen near you.",
        options: [
-        {
-            text: "Take Pen.",
-            setState: { pen: true },
-            nextText: 2
+        {   text: "Take Pen.", setState: { pen: true }, nextNode: Node.merchant},
+        {   text: "Leave the pen.", setState: {pen: false}, nextNode: Node.merchant},
+            ] 
         },
-        {
-            text: "Leave the pen.",
-            //setState: {pen: false},
-            nextText: 2
-        }
-       ] 
-    },
-    {
-        id: 2,
+    
+    [Node.merchant] : {
         text: "You venture forth in search of answers to where you are when you come across a merchant.",
         options: [
-            {
-                text: "Trade the pen for a sword.",
+            {text: "Trade the pen for a sword.", setState: {pen: false, sword: true}, 
                 requiredState: (currentState) => currentState.pen,
-                setState: {pen: false, sword: true},
-                nextText: 3
+                nextNode: Node.sword
             },
-            {
-                text: "Trade the pen for a shield.",
+            {text: "Trade the pen for a shield.", setState: {pen: false, shield: true},
                 requiredState: (currentState) => currentState.pen,
-                setState: {pen: false, shield: true},
-                nextText: 3
+                nextNode: Node.sword
+            },
+            {text: "Trade the pen for a sword and shield.", setState: {pen: false, sword: true, shield: true},
+                requiredState: (currentState) => currentState.pen && currentState.shield,
+                nextNode: Node.sword
             },
             {
                 text: "Ignore the merchant.",
-                nextText: 3
+                nextNode: Node.lose
+            } 
+        ]
+    },
+    [Node.sword] : {
+        text: "You have intrusive thoughts about killing the merchant.",
+        options: [
+            {text: "Attack the merchant.", setState: {attack:true, merchantHealth: -100},
+                requiredState: (currentState) => currentState.sword,
+               textNode: Node.merchant_dead
+            },
+
+            {text: "Go away.", setState: {attack: false},
+            requiredState: (currentState) => currentState.sword,
+            nextNode: Node.start
             }
         ]
-    }
-]
-
-startGame();
-
-// Factory Function Base
-/*function createCar() {
-    return {
-        brand: "BMW",
-        drive() {
-            console.log("Vroom!")
-        }
+    },
+    [Node.merchant_dead] : {
+        text: "The merchant is dead. You have won the game.",
+        options: [
+            {text: "Play again.", setState: {attack: false, merchantHealth: 100},
+                requiredState: (currentState) => currentState.sword,
+               nextNode: Node.start
+            },
+        ]
     }
 }
 
-const car = createCar()
-car.drive() */
+startGame();
 
+
+    /* {
+        id: 3,
+        text: "You have a sword and a shield. What do you want to do?",
+        options: [
+            {
+                text: "Attack the merchant.",
+                requiredState: (currentState) => currentState.sword && currentState.shield,
+                setState: {attack:true, merchantHealth: -100},
+                nextNode: 4
+            },
+            {
+                text: "Go away.",
+                requiredState: (currentState) => currentState.sword && currentState.shield,
+                setState: {attack: false},
+                nextNode: 5
+            }
+        ]
+    },
+    {
+        id: 4,
+        text: "You have a sword and a shield. What do you want to do?",
+        options: [
+            {
+                text: "Attack the merchant.",
+                requiredState: (currentState) => currentState.sword && currentState.shield,
+                setState: {attack:true, merchantHealth: -100},
+                nextNode: 5
+            },
+            {
+                text: "Go away.",
+                requiredState: (currentState) => currentState.sword && currentState.shield,
+                setState: {attack: false},
+                nextNode: 4
+            }
+        ]
+    },
+    {
+        id: 5,
+        text: "You have a sword and a shield. What do you want to do?",
+        options: [
+            {
+                text: "Attack the merchant.",
+                requiredState: (currentState) => currentState.sword && currentState.shield,
+                setState: {attack:true, merchantHealth: -100},
+                nextNode: 1
+            },
+            {
+                text: "Go away.",
+                requiredState: (currentState) => currentState.sword && currentState.shield,
+                setState: {attack: false},
+                nextNode: 1
+            }
+        ]
+    } */
