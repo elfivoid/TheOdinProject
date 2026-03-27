d96f2ddcbf8f7681077f31fccb92f11988bead78
elfivoid <elisa@aulweb.de>
2026-03-27 23:40:19 +0100

    Character Creation with JS Prototypes
    - js prototypes
    - prototype inheritance
    - constructor functions

--- javascriptfullstack/JavaScript/object_constructors/object.js
+++ javascriptfullstack/JavaScript/object_constructors/object.js
@@ -57,3 +57,46 @@ Player.prototype.sayHello = function() {
 
 player1.sayHello(); // logs "Hello, I'm a player!"
 player2.sayHello(); // logs "Hello, I'm a player!"
+
+// ------------------ Character Creation -----------------------------------------------
+
+let x = {};
+console.log(Object.getPrototypeOf(x)); //to find the newly created object
+
+// Initialize constructor functions
+function Hero(name, level) {
+  this.name = name;
+  this.level = level;
+}
+
+function Warrior(name, level, weapon) {
+  Hero.call(this, name, level);
+
+  this.weapon = weapon;
+}
+
+function Healer(name, level, spell) {
+  Hero.call(this, name, level);
+
+  this.spell = spell;
+}
+
+// Link prototypes and add prototype methods
+Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
+Object.setPrototypeOf(Healer.prototype, Hero.prototype);
+
+Hero.prototype.greet = function () {
+  return `${this.name} says hello.`;
+}
+
+Warrior.prototype.attack = function () {
+  return `${this.name} attacks with the ${this.weapon}.`;
+}
+
+Healer.prototype.heal = function () {
+  return `${this.name} casts ${this.spell}.`;
+}
+
+// Initialize individual character instances
+const hero1 = new Warrior('Bjorn', 1, 'axe');
+const hero2 = new Healer('Kanin', 1, 'cure');
